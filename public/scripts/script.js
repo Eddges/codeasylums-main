1627517cc40563751eaa6706532360065c53fc63
Eddges <shivamshekhar.i7@gmail.com>
2020-05-30 12:18:16 +0530

    Before pull

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -33,3 +33,100 @@ $(document).ready(function(){
 })
 
 
+
+
+
+const nav = document.querySelector(".fix");
+    const sectionOne = document.querySelector(".intersection")
+
+    const sectionOneOptions = {
+        rootMargin : "100px 0px 0px 0px"
+    };
+
+    const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                // nav.classList.add('nav-scrolled')
+                // console.log(nav.classList)
+                $(".landing-left-text").addClass("landing-left-text-shifted")
+                $(".landing-left .WaveFront").addClass("WaveFront-shifted")
+                $(".landing-left .WaveBack").addClass("WaveBack-shifted")
+                $(".landing-left .Main").addClass("Main-shifted")
+                $(".landing-left .Decor").addClass("Decor-shifted")
+                $('.landing-left').addClass('landing-left-shifted')
+                $('.landing-left .ReactJS').addClass('ReactJS-shifted')
+                $('.landing-left .Kuber').addClass('Kuber-shifted')
+                $('.landing-left .HTML5').addClass('HTML5-shifted')
+                $('.landing-left .Github').addClass('Github-shifted')
+                $('.landing-left .Table').addClass('Table-shifted')
+                $('.topLogoBlack').addClass('topLogoBlack-noshow')
+                $('.topLogoWhite').addClass('topLogoWhite-show')
+                $('.main-bottom-text').addClass('main-bottom-text-shifted-left')
+                $('.topLogo').addClass('topLogo-shifted')
+            }
+            else{
+                $(".landing-left-text").removeClass("landing-left-text-shifted")
+                $(".landing-left .WaveFront").removeClass("WaveFront-shifted")
+                $(".landing-left .WaveBack").removeClass("WaveBack-shifted")
+                $(".landing-left .Main").removeClass("Main-shifted")
+                $(".landing-left .Decor").removeClass("Decor-shifted")
+                $('.landing-left').removeClass('landing-left-shifted')
+                $('.landing-left .ReactJS').removeClass('ReactJS-shifted')
+                $('.landing-left .Kuber').removeClass('Kuber-shifted')
+                $('.landing-left .HTML5').removeClass('HTML5-shifted')
+                $('.landing-left .Github').removeClass('Github-shifted')
+                $('.landing-left .Table').removeClass('Table-shifted')
+                $('.topLogoBlack').removeClass('topLogoBlack-noshow')
+                $('.topLogoWhite').removeClass('topLogoWhite-show')
+                $('.main-bottom-text').removeClass('main-bottom-text-shifted-left')
+                $('.topLogo').removeClass('topLogo-shifted')
+            }
+        })
+    }, sectionOneOptions)
+
+    sectionOneObserver.observe(sectionOne)
+
+
+
+
+    const sectionOneRight = document.querySelector(".intersectionRight")
+
+    const sectionOneRightOptions = {
+        rootMargin : "-100px 0px 0px 0px"
+    };
+    
+
+    const sectionOneObserverRight = new IntersectionObserver(function(entries, sectionOneObserverRight){
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                // nav.classList.add('nav-scrolled')
+                // console.log(nav.classList)
+                $('.landing-right-text').addClass('landing-right-text-shifted')
+                $('.landing-right .WaveFront').addClass('WaveFront-shifted')
+                $('.landing-right .WaveBack').addClass('WaveBack-shifted')
+                $('.landing-right .Main').addClass('Main-shifted')
+                $('.landing-right .Decor').addClass('Decor-shifted')
+                $('.AWS').addClass('AWS-shifted')
+                $('.Docker').addClass('Docker-shifted')
+                $('.Ansible').addClass('Ansible-shifted')
+                $('.landing-right').addClass('landing-right-shifted')
+                $('.main-bottom-text').addClass('main-bottom-text-shifted-right')
+                $('.main-bottom-text-right').addClass('main-bottom-text-right-shifted')
+            }
+            else{
+                $('.landing-right-text').removeClass('landing-right-text-shifted')
+                $('.landing-right .WaveFront').removeClass('WaveFront-shifted')
+                $('.landing-right .WaveBack').removeClass('WaveBack-shifted')
+                $('.landing-right .Main').removeClass('Main-shifted')
+                $('.landing-right .Decor').removeClass('Decor-shifted')
+                $('.AWS').removeClass('AWS-shifted')
+                $('.Docker').removeClass('Docker-shifted')
+                $('.Ansible').removeClass('Ansible-shifted')
+                $('.landing-right').removeClass('landing-right-shifted')
+                $('.main-bottom-text').removeClass('main-bottom-text-shifted-right')
+                $('.main-bottom-text-right').removeClass('main-bottom-text-right-shifted')
+            }
+        })
+    })
+
+    sectionOneObserverRight.observe(sectionOneRight)
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -28,14 +28,22 @@ const Main = () => {
     return(
         
         <div className="landing-container">
+            <div className="intersection"></div>
             <div className="glass"></div>
+            <div className="fix"></div>
+            <div className="topLogo">
+                <img className="topLogoBlack" src={Logo} alt="Logo" />
+                <img className="topLogoWhite" src={LogoWhite} alt="LogoWhite" />
+            </div>
             <div className="landing-left">
                 <div className="landing-left-text">
                     <p>Student?</p>
                     <p>Are you a student</p>
                     <p>have a knack for tech?<br></br>want to crack interviews?</p>
                     <p>Give us and yourself some time and secure your dream job</p>
+                    <div className="intersection"></div>
                     <button className="landing-left-button" onClick={student}>Let’s play & code</button>
+                    
                 </div>
                 <img className="Main" src={Hacker} />
                 <img className="Decor" src={HackerPC} alt="Decor" />
@@ -56,12 +64,14 @@ const Main = () => {
                     <p>Professional?</p>
                     <p>Are you a working professional</p>
                     <p>Want to improve your skills? <br></br> Crack the Coding Interview?</p>
-                   
                     <p>Jumpstart with professional courses from CodeAsylums online Bootcamp</p>
                     <button className="landing-right-button" onClick={prof}>Let’s plan a career transformation</button>
+                    
                 </div>
+                <div className="intersectionRight"></div>
                 <img className="Decor" src={ProfessionalBG} alt="Decor" />
                 <img className="Main" src={Professional1} alt="Main" />
+                <div className="intersectionRight"></div>
                 
                 {/* <img className="Docker" src={Docker} alt="Decor" /> */}
                 {/* <img className="Ansible" src={Ansible} alt="Decor" />
